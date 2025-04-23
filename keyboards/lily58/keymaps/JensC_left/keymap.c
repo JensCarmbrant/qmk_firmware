c8a4bc58d1899c05fce3ed86f4111478f8b7330c
Jens Carmbrant <jens.carmbrant@volvo.com>
2025-04-23 08:12:18 +0200

    Refactor Lily58 keymaps: Split JensC into left and right configurations
        - Deleted original keymap files for JensC.
        - Created separate keymap files for JensC_left and JensC_right.
        - Copied configuration and keymap definitions into new files for both left and right halves.
        - Ensured RGB lighting effects and layer configurations are maintained in the new keymaps.
        - Updated rules.mk for both left and right keymaps to reflect the new structure

--- keyboards/lily58/keymaps/JensC_left/keymap.c
+++ keyboards/lily58/keymaps/JensC_left/keymap.c
@@ -28,15 +28,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_END,  KC_HOME,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_END,   KC_HOME,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                           MO(_LOWER), KC_LGUI, KC_LALT, KC_SPC, KC_ENT, KC_BSPC, KC_RALT, MO(_RAISE)
 ),
 
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  ~   |  !   |  @   |  #   |  $   |  %   |                    |  ]   |  &   |  *   |  (   |  )   | F12  |
+ * |  ~   |  !   |  @   |  #   |  $   |  %   |                    |  ]   |  &   |  *   |  (   |  )   |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |  1   |  2   |  3   |  4   |  5   |-------.    ,-------|  6   |  7   |  8   |  9   |  0   |  \   |
  * |------+------+------+------+------+------| BriDn |    | BriUp |------+------+------+------+------+------|
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  KC_GRV,        KC_F1,      KC_F2,       KC_F3,       KC_F4,        KC_F5,                           KC_F6,       KC_F7,        KC_F8,          KC_F9,         KC_F10,       KC_F11,
-  LSFT(KC_GRV),  KC_EXLM,    RALT(KC_2),  KC_HASH,     RALT(KC_4),   KC_PERC,                         KC_RBRC,     LSFT(KC_6),   LSFT(KC_NUHS),  LSFT(KC_8),    LSFT(KC_9),   KC_F12,
+  KC_F1,         KC_F2,      KC_F3,       KC_F4,       KC_F5,        KC_F6,                           KC_F7,       KC_F8,        KC_F9,          KC_F10,        KC_F11,       KC_F12, 
+  LSFT(KC_GRV),  KC_EXLM,    RALT(KC_2),  KC_HASH,     RALT(KC_4),   KC_PERC,                         KC_RBRC,     LSFT(KC_6),   LSFT(KC_NUHS),  LSFT(KC_8),    LSFT(KC_9),   KC_TRNS,
   KC_TRNS,       KC_1,       KC_2,        KC_3,        KC_4,         KC_5,                            KC_6,        KC_7,         KC_8,           KC_9,          KC_0,         RALT(KC_NUBS),
   KC_TRNS,       RALT(KC_8), RALT(KC_9),  RALT(KC_7),  RALT(KC_0),   RALT(KC_MINS), KC_BRID, KC_BRIU, LSFT(KC_0),  KC_MINS,      KC_SLSH,        KC_UNDS,       LSFT(KC_7),   LSFT(KC_MINS),
                                         KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,        KC_TRNS,      KC_TRNS,       KC_TRNS,        KC_TRNS
@@ -56,45 +56,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  DEL |      |      |      |      |      |                    |      |      |      |      |      | ¨~^  |
+ * |  DEL |      |      |      |      |      |                    |      |      |      |      |  ´`  | ¨~^  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    | PgUp |      |  Up  |      |      |  '*  |
+ * |      |      |      |      |      |      |                    |      |      |  Up  |      |      |  '*  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------| PgDn | Left | Down | Right|      | Whlu |
+ * |      |      |      |      |      |      |-------.    ,-------|      | Left | Down | Right|      |      |
  * |------+------+------+------+------+------| Mute  |    | Mute  |------+------+------+------+------+------|
- * |  <   |  >   |  |   |      |      |      |-------|    |-------|      |      |      |      |      | Whld |
+ * |  <   |  >   |  |   |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_RAISE] = LAYOUT(
-  KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RBRC,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                               KC_PGUP, KC_TRNS, KC_UP,   KC_TRNS,  KC_TRNS, KC_BSLS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                               KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, MS_WHLU,
-  KC_NUBS, LSFT(KC_NUBS), RALT(KC_NUBS), KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MS_WHLD,
+  KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_EQL, KC_RBRC,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,  KC_TRNS, KC_BSLS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                               KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_TRNS,
+  KC_NUBS, LSFT(KC_NUBS), RALT(KC_NUBS), KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* ADJUST
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * | BOOT |      |      |      |      |      |                    |      |      |      |      |      | BOOT |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |RGB_TOG|-------.    ,-------|      |      |      |      |      |      |
- * |------+------+------+------+------+------| RGB_TOG|    |RGB_TOG|------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * ,-----------------------------------------.                     ,-----------------------------------------.
+ * |      |      |      |      |      |      |                     |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                     | PgUp |      | Whlu |      |      |      |
+ * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------.     ,-------| PgDn |      | Whld |      |      |      |
+ * |------+------+------+------+------+------|       |     |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|     |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
   [_ADJUST] = LAYOUT(
-  QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, RGB_TOG,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_PGUP, KC_TRNS, MS_WHLU, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_PGDN, KC_TRNS, MS_WHLD, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
